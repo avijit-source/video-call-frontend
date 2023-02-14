ea1209c7f68a023cf1d0a5fb9db07c7367bb6561
avijit-source <79460627+avijit-source@users.noreply.github.com>
2023-02-14 22:50:06 +0530

    Update CallPage.js

--- src/views/CallPage.js
+++ src/views/CallPage.js
@@ -22,7 +22,40 @@ import { v4 as uuidv4 } from 'uuid';
 
 const ENDPOINT = "https://video-call-backend-avijitweb.onrender.com";
 var socket;
-
+const ICESERVER = [{ url: 'stun:stun01.sipphone.com' },
+{ url: 'stun:stun.ekiga.net' },
+{ url: 'stun:stun.fwdnet.net' },
+{ url: 'stun:stun.ideasip.com' },
+{ url: 'stun:stun.iptel.org' },
+{ url: 'stun:stun.rixtelecom.se' },
+{ url: 'stun:stun.schlund.de' },
+{ url: 'stun:stun.l.google.com:19302' },
+{ url: 'stun:stun1.l.google.com:19302' },
+{ url: 'stun:stun2.l.google.com:19302' },
+{ url: 'stun:stun3.l.google.com:19302' },
+{ url: 'stun:stun4.l.google.com:19302' },
+{ url: 'stun:stunserver.org' },
+{ url: 'stun:stun.softjoys.com' },
+{ url: 'stun:stun.voiparound.com' },
+{ url: 'stun:stun.voipbuster.com' },
+{ url: 'stun:stun.voipstunt.com' },
+{ url: 'stun:stun.voxgratia.org' },
+{ url: 'stun:stun.xten.com' },
+{
+    url: 'turn:numb.viagenie.ca',
+    credential: 'muazkh',
+    username: 'webrtc@live.com'
+},
+{
+    url: 'turn:192.158.29.39:3478?transport=udp',
+    credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+    username: '28224511:1379330808'
+},
+{
+    url: 'turn:192.158.29.39:3478?transport=tcp',
+    credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+    username: '28224511:1379330808'
+}]
 function CallPage() {
     const params = useParams()
     const location = useLocation()
@@ -53,7 +86,11 @@ function CallPage() {
         // });
 
 
-        myPeer.current = new Peer(location.state + uuidv4());
+        myPeer.current = new Peer(location.state + uuidv4(), {
+            config: {
+                'iceServers': ICESERVER
+            }
+        });
 
 
         async function getMedia() {
